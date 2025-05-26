dba4a5fcbbe34d8228edef202fc726a626b7b23b
ayagri1632 <yeminhtet.1632@gmail.com>
2025-05-26 12:11:13 +0630

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,11 @@
+src/App.js
+function App() {
+  return (
+    <div>
+      <h1>Welcome to AY Agri App</h1>
+      <p>This app helps identify plant diseases from images and recommends suitable treatments.</p>
+    </div>
+  );
+}
+
+export default App;
